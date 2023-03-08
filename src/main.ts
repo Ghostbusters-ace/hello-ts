ecba03a53739f6adae61bc728e8f1687a34edc47
lionj <judin.malivert@poe.aelion.biz>
2023-03-08 11:28:45 +0100

    creat form who read and if 4 condition is meet

---
+++ src/main.ts
@@ -0,0 +1,60 @@
+import { HTMLTable } from './html-elements/html-table'
+import { ListHTML } from './html-elements/list-html'
+/**
+ * main.ts
+ * @author Aélion <jean-luc.aubert@aelion.fr>
+ * @version 1.0.0
+ * 
+ * Entry point of our frontend application
+ */
+class Main {
+    constructor() {
+        let myName: string
+        myName = 'Jean-Luc'
+
+        /**
+        * Récupère dans le DOM (Document Object Model) le premier Objet (élément HTML)
+        * qui dispose d'un attribut "app"
+        */
+        const app: Element = document.querySelector('[app]')
+        app.innerHTML = myName
+
+        // New instance of ListHTML
+        const listHTML = new ListHTML()
+        listHTML.build()
+
+        //Instance of HTMLTable
+
+        this.studentList().then(
+            (datas:any) => {
+                const names: Array<string> = datas
+                .map((data:any) => data.lastName)
+
+                const tableEl: HTMLTable = new HTMLTable()
+        tableEl.setTableContent(names)
+        app.appendChild(tableEl.build())
+        })
+
+        
+    
+    }
+
+    private studentList(): Promise<any> {
+
+        const endPoint:string = 'http://127.0.0.1:5000/api/v1/students'
+        return fetch(
+            endPoint
+        ).then((response:Response) =>{
+            return response.json()
+        })
+    }
+}
+
+/**
+ * Launch app
+ */
+const main = new Main()
+
+
+    console.log('Keyup was detected')
+}
